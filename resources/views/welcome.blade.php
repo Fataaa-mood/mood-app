d71715f4829f1159bcd71dbbbc33f0dfa6e888fd
Thierno Diallo <78595975+thierno1492@users.noreply.github.com>
2021-08-19 10:51:48 +0200

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Laravel8</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -25,7 +25,7 @@
             <vue-example-name>
             </vue-example-name>
         </div>
-        
+
         <script src="{{asset('js/app.js')}}">
         </script>
     </body>
