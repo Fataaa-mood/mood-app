d32ae9e6b15359354c7deedffb3e03f0bd1d0299
Fresh! <francocalvino89@gmail.com>
2021-08-19 18:33:37 +0200

    rutas en blade con componentes

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,12 @@ Vue.component('vue-example-name', require('./components/VueExample.vue').default
 
 Vue.component('vue-selection', require('./components/VueSelection.vue').default);
 
+Vue.component('vue-intro', require('./components/VueIntro.vue').default);
+
+Vue.component('vue-characters', require('./components/VueCharacters.vue').default);
+
+Vue.component('vue-stages', require('./components/VueStages.vue').default);
+
 const app = new Vue ({
     el: '#app'
 });
