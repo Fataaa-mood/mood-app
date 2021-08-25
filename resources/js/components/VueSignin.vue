94d619316e8df23d8a6b4565ba94d1d51026ae21
Andres Patiño <andresp199519@gmail.com>
2021-08-25 12:00:06 +0200

    Se cambió el fondo estático a uno con gif

--- resources/js/components/VueSignin.vue
+++ resources/js/components/VueSignin.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-5 row justify-content-center mt-4">
+            <div class="col-md-5 row justify-content-center mt-4 mb-5 border-0">
                 <div class="label p-2">
                     <h5>Login to Start Playing</h5>
                 </div>
-                <div class="form-div p-2 row justify-content-center mt-3 mb-3">
+                <div class="form-div p-2 row justify-content-center mt-3 mb-3 border-0">
                     <b-form @submit="onSubmit">
                         <b-form-group
                             id="input-group-1"
@@ -34,8 +34,8 @@
                         <b-button type="reset" variant="danger">Reset</b-button>
                     </b-form>
                 </div>
-                <div>
-                    <h6>Create a New Account</h6>
+                <div class="create-account mt-3 text-white shadow ">
+                    <h5>Create a New Account</h5>
                 </div>
     
             </div>
@@ -56,8 +56,9 @@
 </script>
 
 <style scoped>
+
 .container {
-    background-image: url('../../../public/storage/background-login.png');
+    background-image: url('../../../public/storage/background-login-2.gif');
     background-size: 100% 100%;
     background-repeat: no-repeat;
     height: 100vh;
@@ -65,14 +66,31 @@
     margin: 0px;
 }
 
-.form-div {
-    background-color: white;
-    height: 45vh;
-    width: 50vh;
-    
+.label  {
+    margin-bottom: 30px;
+    background:rgba(255,255,255,0.85);
+    border: none !important;
+    -webkit-box-shadow: 0px 0px 22px 20px rgba(255,255,255,0.8); 
+    box-shadow: 0px 0px 22px 20px rgba(255,255,255,0.8);
+    border-radius:10px;
 }
 
-.label  {
-    background-color: white;
+.form-div {
+    background:rgba(255,255,255,0.85);
+    height: 45vh;
+    width: 50vh;
+    border: none !important;
+    -webkit-box-shadow: 0px 0px 22px 20px rgba(255,255,255,0.8); 
+    box-shadow: 0px 0px 22px 20px rgba(255,255,255,0.8);
+    border-radius:10px;
+    outline:none;
+
+
 }
+
+.create-account{
+    font-style: ; 
+    filter: drop-shadow(5px 5px 4px white ); 
+}
+
 </style>
