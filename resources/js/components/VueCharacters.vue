f42040d339200703205de86e7349bdb4ed8e7828
PPIICCAA <abdulhaidrees@gmail.com>
2021-08-27 08:54:41 +0200

    character

--- resources/js/components/VueCharacters.vue
+++ resources/js/components/VueCharacters.vue
@@ -1,18 +1,55 @@
 <template>
-    <div class="d-flex justify-content-center">
-            <div class="ch0">
-                <img :src="require('../../../public/storage/images/character0.jpg').default"/>
+    <div class="containerCharacters d-flex justify-content-center align-items-center">
+            <div class="incognite d-flex align-items-center flex-column">
+                <img class="ch0" :src="require('../../../public/storage/images/character0.jpg').default"/>
                 <h2>Who are you?</h2>
             </div>
-            <div></div>
+            <div class="d-flex justify-content-center align-items-center flex-wrap">
+                <div class="d-flex align-items-center flex-column">
+                    <img class="selectCharacter" :src="require('../../../public/storage/images/ch1.png').default"/>
+                    <p>The Outlier</p>
+                </div>
+                <div class="d-flex align-items-center flex-column">
+                    <img class="selectCharacter" :src="require('../../../public/storage/images/ch2.png').default"/>
+                    <p>The Stronger</p>
+                </div>
+                <div class="d-flex align-items-center flex-column">
+                    <img class="selectCharacter" :src="require('../../../public/storage/images/ch3.png').default"/>
+                    <p>The Rebel</p>
+                </div>
+                <div class="d-flex align-items-center flex-column">
+                    <img class="selectCharacter" :src="require('../../../public/storage/images/ch4.png').default"/>
+                    <p>The Saviour</p>
+                </div>
+                <div class="d-flex align-items-center flex-column">
+                    <img class="selectCharacter" :src="require('../../../public/storage/images/ch5.png').default"/>
+                    <p>The Diva</p>
+                </div>
+            </div>
     </div>
 </template>
 <style scoped>
+.selectCharacter {
+    height: 25vh;
+    margin: 3vh;
+}
+.containerCharacters {
+    background-color: #202020;
+    height: 100vh;
+}
+.incognite {
+    margin-left: 10vh;
+}
 .ch0 {
-    /* background-image: url("../../../public/storage/images/character0.jpg"); */
-    background-size: 100% 100%;
-    background-repeat: no-repeat;
-    height: 70vh;
+    height: 80vh;
+}
+p {
+    color: white;
+    font-size:  10px;
+}
+h2 {
+    color: white;
+    font-size: 20px;
 }
 </style>
 <script>
