217af492e41480f311fdb60fb35049e79402aa62
Thierno Diallo <78595975+thierno1492@users.noreply.github.com>
2021-08-30 11:39:09 +0200

    agregando compo map

--- routes/web.php
+++ routes/web.php
@@ -40,6 +40,9 @@ Route::get('/characters', function () {
 Route::get('/stages', function () {
     return view('stages');
 });
+Route::get('/map', function () {
+    return view('map');
+});
 
 
 require __DIR__.'/auth.php';
