39fefdda8fc0797d285a9f50f3af680064d72dab
PPIICCAA <abdulhaidrees@gmail.com>
2021-09-03 09:49:43 +0200

    missions

--- resources/js/components/VueMissionNormal.vue
+++ resources/js/components/VueMissionNormal.vue
@@ -5,19 +5,20 @@
             <h3>Señala cuál de estas son estrategias de los videojuegos para engancharnos!</h3>
         </div>
         <div class="containerQuestions">
-                <div class="question d-flex align-items-center flex-column">
+                <div @click="borderActive($event)" class="question d-flex align-items-center flex-column">
                     <p>Premios instantáneos</p>
                 </div>
-                <div class="question d-flex align-items-center flex-column">
+                <div @click="borderActive($event)" class="question d-flex align-items-center flex-column">
                     <p>Poder jugar varios modos e infinito</p>
                 </div>
-                <div class="question d-flex align-items-center flex-column">
+                <div @click="borderActive($event)" class="question d-flex align-items-center flex-column">
                     <p>Compras dentro del videojuego para mejorar y llegar más alto</p>
                 </div>
-                <div class="question d-flex align-items-center flex-column">
+                <div @click="borderActive($event)" class="question d-flex align-items-center flex-column">
                     <p>El precio inicial del juego</p>
                 </div>
             </div>
+            <button @click="nextQuestion" class="buttonNext">NEXT</button>
     </div>
 </template>
 
@@ -29,37 +30,142 @@ export default {
     },
     data() {
         return {
-        display: block,
         };
     },
     methods: {
-
         movedisplay() {
             setTimeout(() => {
                 document.querySelector(".cover-contain div").classList.add("titleSmall")
                 document.querySelector(".cover-contain div").classList.remove("titleBig")
                 document.querySelector(".containerQuestions").style.opacity = "1"
                 document.querySelector(".containerQuestions").style.visibility = "visible"
-                document.querySelector(".selectCharacter").style.visibility = "visible"
+                document.querySelector(".selectCharacter").style.opacity = "1"
+                document.querySelector(".buttonNext").style.opacity = "0.5"
             },2000)
-        }  
+        },
+        borderActive(event) {
+            let divs = document.querySelectorAll(".containerQuestions div")
+            for (let i = 0; i < divs.length; i++) {
+                    divs[i].classList.remove("red")                    
+                    document.querySelector(".buttonNext").style.opacity = "1"
+            }            
+            event.path[1].classList.add("red")
+        },
+        nextQuestion() {
+            let divs = document.querySelectorAll(".containerQuestions div")
+            for (let i = 0; i < divs.length; i++) {
+                    if(divs[i].classList.contains("red")) {
+                        let result = divs[i].querySelector("p").innerHTML
+                        console.log(result)
+                    }
+            }
+        }
     },
 }
 </script>
 <style scoped>
+.buttonNext, .buttonNext::after {
+  width: 10vw;
+  height: 12vh;
+  font-family: 'Bebas Neue', cursive;
+  background: linear-gradient(45deg, transparent 5%, #0b7745 5%);
+  border: 0;
+  color: #fff;
+  letter-spacing: 3px;
+  line-height: 20px;
+  box-shadow: 6px 0px 0px #03532b;
+  outline: transparent;
+  position: relative;
+}
+
+.buttonNext::after {
+  content: 'AVAILABLE NOW';
+  display: block;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: linear-gradient(45deg, transparent 3%, #0b7745 3%, #0b7745 5%, #03532b 5%);
+  text-shadow: -3px -3px 0px #F8F005, 3px 3px 0px #0b7745;
+  clip-path: inset(50% 50% 50% 50%);
+}
+
+.buttonNext:hover::after {
+  animation: 1s glitch;
+  animation-timing-function: steps(2, end);
+}
+
+@keyframes glitch {
+  0% {
+    clip-path: inset(80% -6px 0 0);
+    transform: translate(-20px, -10px);
+  }
+  10% {
+    clip-path: inset(50% -6px 30% 0);
+    transform: translate(10px, 10px);
+  }
+  20% {
+    clip-path: inset(80% -6px 0 0);
+    transform: translate(-10px, 10px);
+  }
+  30% {
+    clip-path: inset(10% -6px 85% 0);
+    transform: translate(0px, 5px);
+  }
+  40% {
+    clip-path: inset(50% -6px 30% 0);
+    transform: translate(-5px, 0px);
+  }
+  50% {
+    clip-path: inset(10% -6px 85% 0);
+    transform: translate(5px, 0px);
+  }
+  60% {
+    clip-path: inset(40% -6px 43% 0);
+    transform: translate(5px, 10px);
+  }
+  70% {
+    clip-path: inset(50% -6px 30% 0);
+    transform: translate(-10px, 10px);
+  }
+  80% {
+    clip-path: inset(80% -6px 5% 0);
+    transform: translate(20px, -10px);
+  }
+  90% {
+    clip-path: inset(80% -6px 0 0);
+    transform: translate(-10px, 0px);
+  }
+  100% {
+    clip-path: inset(80% -6px 0 0);
+    transform: translate(0);
+  }
+}
+.buttonNext {
+    position: absolute;
+    right: 20px;
+    bottom: 20px;
+    opacity: 0;
+    transition: opacity 5s linear;
+}
+.red {
+    border: red solid 1px;
+}
 .selectCharacter{
     position: absolute;
-    visibility: hidden;
+    opacity: 0;
     right: 20px;
     top: 20px;
     border-radius: 20px;
     border: rgba(255, 255, 255, 0.5) solid;
-    transition: visibility 5s, opacity 5s linear;
+    transition: opacity 10s linear;
 }
 .containerQuestions {
+    border: none !important;    
     opacity: 0;
     visibility: hidden;    
-    transition: visibility 5s, opacity 5s linear;
+    transition: visibility 5s, opacity 10s linear;
     
 }
 .cover-contain{
